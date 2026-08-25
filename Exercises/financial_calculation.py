74c83400ee9ec7343312c1a6b0fe313cd59a6f81
Júlia Monteiro Santos <a01723519@tec.mx>
2026-08-25 06:54:49 +0000

    Complete Activity 1 Codespaces practice

---
+++ Exercises/financial_calculation.py
@@ -0,0 +1,12 @@
+present_value = 1200
+interest_rate = 0.07
+periods = 11
+
+
+future_value = present_value * (1 + interest_rate) ** periods
+
+
+print("Present value:", present_value)
+print("Interest rate:", interest_rate)
+print("Periods:", periods)
+print("Future value:", round(future_value, 2))
---
+++ Exercises/hello_world.py
@@ -0,0 +1,5 @@
+student_name = "Júlia Monteiro Santos"
+
+print("Hello, World!")
+print("Welcome,", student_name)
+print("Course: Algorithms and Data Analysis")
